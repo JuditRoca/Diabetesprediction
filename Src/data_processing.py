7fffa95bc646d246a42d1171e449b6df9b72d53a
JuditRoca <roca.tovar@gmail.com>
2023-06-24 12:25:39 +0200

    modificacion variables 24.06

--- Src/data_processing.py
+++ Src/data_processing.py
@@ -10,24 +10,34 @@ tasa_cambio = 0.92
 
 df_original["salario_eur"] = df_original["salary_in_usd"] * tasa_cambio
 
-#groupby experiencia y salarios
+#sustituimos variables categoricas por el salario medio de cada tipo.
 
-exp_sal = df_original.groupby('experience_level')['salario_eur'].mean().round(2)
+mean_encoding_experience_level = df_original.groupby('experience_level')['salario_eur'].mean()
+df_original["experience_level_mean"] = df_original['experience_level'].map(mean_encoding_experience_level)
 
-# Reemplazar las etiquetas por los salarios medios correspondientes
-df_original['experience_level'] = df_original['experience_level'].map(exp_sal)
+mean_encoding_employment_type = df_original.groupby('employment_type')['salario_eur'].mean()
+df_original["employment_type_mean"] = df_original['employment_type'].map(mean_encoding_employment_type)
 
-# Mapear las etiquetas de tamaño de empresa a valores numéricos
-mapping = {'S': 1, 'M': 2, 'L': 3}
-df_original['company_size'] = df_original['company_size'].map(mapping)
+mean_encoding_job_title = df_original.groupby('job_title')['salario_eur'].mean()
+df_original["job_title_mean"] = df_original['job_title'].map(mean_encoding_job_title)
 
-#encoder label de employment_type y job_title
-from sklearn.preprocessing import LabelEncoder
-cols = ['employment_type','job_title','employee_residence','company_location']
-df_original[cols]=df_original[cols].apply(LabelEncoder().fit_transform)
+mean_encoding_company_location = df_original.groupby('company_location')['salario_eur'].mean()
+df_original["company_location_mean"] = df_original['company_location'].map(mean_encoding_company_location)
 
-#Eliminar variables redundantes.
-df_original.drop(columns=["salary","salary_currency","salary_in_usd"], inplace=True)
+mean_encoding_employee_location = df_original.groupby('employee_residence')['salario_eur'].mean()
+df_original["employee_location_mean"] = df_original['employee_residence'].map(mean_encoding_employee_location)
+
+mean_encoding_company_size = df_original.groupby('company_size')['salario_eur'].mean()
+df_original["company_size_mean"] = df_original['company_size'].map(mean_encoding_company_size)
+
+mean_encoding_remote_ratio = df_original.groupby('employment_type')['salario_eur'].mean()
+df_original["remote_ratio_mean"] = df_original['employment_type'].map(mean_encoding_remote_ratio)
+
+#asignamos variables a df_processed
+
+df_processed = df_original[['work_year','salario_eur', 'experience_level_mean', 'employment_type_mean',
+       'job_title_mean', 'company_location_mean', 'employee_location_mean',
+       'company_size_mean', 'remote_ratio_mean']]
 
 #funcion crear csv
 
@@ -38,4 +48,4 @@ def csv(nombre_archivo, archivo_guardar):
     archivo_guardar.to_csv(ruta_archivo, index=False)
 
 #extraemos processed
-csv("processed", df_original)
+csv("processed", df_processed)
